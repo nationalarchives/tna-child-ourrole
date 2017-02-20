3201d1dec2bd47001d8860ff6ac484f0a1999664
Punal Chotrani <punalsc@gmail.com>
2017-02-20 12:37:24 +0000

    Added sections of the js into one document ready.

--- js/long-form.js
+++ js/long-form.js
@@ -2,18 +2,16 @@
  * Created by pchotrani on 17/03/16.
  */
 
-$(document).ready(function(){
-
-
+$(document).ready(function () {
     /* Parallax Scrolling */
 
     // Start listening for the user to scroll...
 
-    $(window).scroll(function() {
+    $(window).scroll(function () {
         // Parallax the header with css3 tech
-        if ( $(window).width() > 1024) {
+        if ($(window).width() > 1024) {
             $('.intro-text').css({
-                transform: "translate(0px,-" + $(window).scrollTop() /3 + "px)"
+                transform: "translate(0px,-" + $(window).scrollTop() / 3 + "px)"
             });
         }
         else {
@@ -27,14 +25,12 @@ $(document).ready(function(){
     $("div.wp-caption").removeAttr("style");
 
 
-});
-
-$(document).ready(function(){
+    //Start of scroll script
 
     var $sections = $('.cd-section');
 
     // The user scrolls
-    $(window).scroll(function(){
+    $(window).scroll(function () {
 
         // currentScroll is the number of pixels the window has been scrolled
         var currentScroll = $(this).scrollTop();
@@ -43,29 +39,26 @@ $(document).ready(function(){
         var $currentSection = $(this);
 
         // We check the position of each of the sections compared to the windows scroll positon
-        $sections.each(function(){
+        $sections.each(function () {
             // sectionPosition is the position down the page in px of the current section we are testing
             var sectionPosition = $(this).offset().top;
 
-            // If the sectionPosition is less the the currentScroll position the section we are testing has moved above the window edge.
             // the -1 is so that it includes the section 1px before the section leave the top of the window.
-            if( sectionPosition - 1 < currentScroll ){
-                // We have either read the section or are currently reading the section so we'll call it our current section
+            if (sectionPosition - 1 < currentScroll) {
+                // If the section has either been read or currently reading the section so we'll call it our current section
                 $currentSection = $(this);
-
                 // If the next section has also been read or we are currently reading it we will overwrite this value again. This will leave us with the LAST section that passed.
             }
-
             // This is the bit of code that uses the currentSection as its source of ID
             var id = $currentSection.attr("id");
             $('.cd-menu').removeClass('is-selected');
-            $("[href=#"+id+"]").addClass('is-selected');
+            $("[href=#" + id + "]").addClass('is-selected');
         })
 
     });
 
     /*Scroll to script*/
-    $(".cd-menu").click(function(e) {
+    $(".cd-menu").click(function (e) {
         e.preventDefault();
         var link = $(this).attr('href');
         $('html, body').animate({
@@ -79,10 +72,10 @@ $(document).ready(function(){
     $("<div class='position-top-right'><span class='sprite icon-new-window'></span></div>").insertBefore(".wp-caption > a > img");
 
     /*Fade right navigation when reaches footer*/
-    $(window).scroll(function() {
-        if ($(window).scrollTop() + 600 > $(document).height() - $(window).height() ) {
+    $(window).scroll(function () {
+        if ($(window).scrollTop() + 600 > $(document).height() - $(window).height()) {
             jQuery('#cd-vertical-nav').fadeOut(1000);
-        }else{
+        } else {
             $('#cd-vertical-nav').fadeIn(1000);
         }
     });
